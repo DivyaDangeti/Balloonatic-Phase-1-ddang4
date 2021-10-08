b4172819d4fdab9195b46f6fd4435a5bec1829df
DivyaDangeti <ddang4@unh.newhaven.edu>
2021-10-08 14:46:17 -0400

    My first commit for Webdatabase Project

---
+++ Divya Dangeti-The Ballonatic Asylum /button.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    window.location.assign("homepage.html");
+}
+
+function myProduct() {
+    window.location.assign("productpage.html");
+}
+
+function myAbout() {
+    window.location.assign("aboutPage.html");
+}
+
+function myContact() {
+    window.location.assign("contactPage.html");
+}
